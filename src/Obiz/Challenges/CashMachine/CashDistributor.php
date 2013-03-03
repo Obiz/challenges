85be5f978fad402918cf210ed19322382113ecdf
Julien Roubieu <julien@obiz.com.br>
2013-03-03 15:28:05 -0300

    Inverted expected returned array order

--- src/Obiz/Challenges/CashMachine/CashDistributor.php
+++ src/Obiz/Challenges/CashMachine/CashDistributor.php
@@ -12,7 +12,7 @@ class CashDistributor
     /**
      * Returns the bills that should be distributed for a given withdraw amount and available bills,
      * MINIMIZING the total number of distributed bills.
-     * Ex: getBills(72) => array(2 => 1, 20 => 1, 50 => 1).
+     * Ex: getBills(72) => array(50 => 1, 20 => 1, 2 => 1).
      *
      * @param int $withdrawAmount How much we want to withdraw from the cash distributor
      * @throws InvalidWithdrawException if the exact amount cannot be gathered with the available bills.
